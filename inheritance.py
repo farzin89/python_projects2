cf4c9cc7ea50d753dd4144879af1111b9cab96ef
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-07 09:43:25 +0200

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+from people import Student
+from people import Teacher
+
+student1 = Student('farzin','Ahmadi',33)
+teacher2 = Teacher('luigi','martirano',48)
+
+student1.print_first_name()
+student1.print_info()
+student1.print_first_name()
+teacher2.print_info()
