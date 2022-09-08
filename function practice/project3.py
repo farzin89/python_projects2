3a39600066da594b4d709994b63f79dd15c4d0cf
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-08 07:39:27 +0200

    counting just one space between two words

--- function practice/project3.py
+++ function practice/project3.py
@@ -11,13 +11,23 @@ def printList(list1):
         print(item,end="\t")
     showStar()
 
+
 def wordLen(str1):
     list1 = list(str1)
     wl = 1
-    for item in list1:
-        if item ==' ':
-            wl+=1
+    for i in range(0,len(list1)):
+        if(list1[i] == ' ' and list1[i+1] != ' '):
+           wl += 1
     return wl
+
+#def wordLen(str1):
+#    list1 = list(str1)
+#     wl = 1
+#     for item in list1:
+#       if item ==' ':
+#
+#           wl+=1
+#  return wl
 
 
 str1 = input("Enter Text : ")
