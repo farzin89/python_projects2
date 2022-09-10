4d00c91e06c07f9bc11bf5938e9c88d854a1f012
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-10 08:33:47 +0200

    rough number

--- function practice/short_if.py
+++ function practice/short_if.py
@@ -1,13 +1,27 @@
 
-num = int(input("Enter Number : "))
-temp = num
+x = int(input("Enter Number : "))
+for num in range(1,x):
 
-sum = 0
-while num > 0 :
-    sum += num % 10
-    num//=10
+    temp = num
+    sum = 0
+    while num > 0:
+        sum+= num % 10
+        num//= 10
+    if temp % sum == 0:
+        print(temp, end='\t')
+
+
+
+
+#num = int(input("Enter Number : "))
+#temp = num
+
+#sum = 0
+#while num > 0 :
+    #sum += num % 10
+    #num//=10
 
 
 # this if code is for two condition
-print("Yes") if temp % sum == 0 else print('No')
+#print("Yes") if temp % sum == 0 else print('No')
 
