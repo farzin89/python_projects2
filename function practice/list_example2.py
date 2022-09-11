3d1453b712bcbffd91340a0c29089a6656229deb
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-11 13:51:27 +0200

    sublist

--- function practice/list_example2.py
+++ function practice/list_example2.py
@@ -44,15 +44,30 @@ def myRevers(list9):
 list9 =[12,34,56,7,78,88,90,642]
 print("revers :",myRevers(list9))
 
+#...................................................
+
+# sublist
+
+def subList(List10):
+    list11 =[]
+    for i in range(0,len(list10)+1):
+        for j in range(0,i):
+            list11.append(list10[j:i])
+    return list11
 
 
+list10 = [1,2,3]
+list11 = subList(list10)
+print(list11)
 
+
+#..................................................
 #list7 = []
 #for l in range(1,6):
 #    list7.append(int(input("Enter Number : ")))
 #print(list7)
 
-#...............................................
+#..................................................
 
 list8 =[]
 while True:
