c6e3de233fa408fe53584c41408317212d9dce77
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-12 07:21:43 +0200

    hide character for Name

--- function practice/game1.guess_letter.py
+++ function practice/game1.guess_letter.py
@@ -8,6 +8,18 @@ def filNameList():
 def getRandomName(list1):
     return random.choice(list1)
 
+def hideCharForName(selectName):                          #mehdi
+    len1 = len(selectName)                                # l=5
+    index = random.randint(0,len1-1)                      #index =3
+    newName=selectName[:index]+'_'+selectName[index+1:]
+    print(newName)
+
+
+names = filNameList()
+selectName = getRandomName(names)
+print(selectName)
+hideCharForName(selectName)
+
 
 
 
