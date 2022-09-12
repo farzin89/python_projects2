d2d542e8801a09b79d0500a83dbcd825f937be9b
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-12 07:15:23 +0200

    write two functions, first one for filling the list name and second one for getting the random Name

--- function practice/game1.guess_letter.py
+++ function practice/game1.guess_letter.py
@@ -1,5 +1,13 @@
 
 import random
 
+def filNameList():
+    list1=["ali","reza","mehdi","ahmad","sara","mohammad"]
+    return list1
+
+def getRandomName(list1):
+    return random.choice(list1)
+
+
 
 
