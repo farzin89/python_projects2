a03aa154ab50f1019555fbbb3316689df726cc56
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-12 08:00:04 +0200

    start to play

--- function practice/game1.guess_letter.py
+++ function practice/game1.guess_letter.py
@@ -12,13 +12,24 @@ def hideCharForName(selectName):                          #mehdi
     len1 = len(selectName)                                # l=5
     index = random.randint(0,len1-1)                      #index =3
     newName=selectName[:index]+'_'+selectName[index+1:]
+    hideChar = selectName[index]
     print(newName)
+    return hideChar
+
+def playing(selectName,hideChar):
+    count = 0
+    for i in range(0,len(selectName)):
+        ch =input("Enter char : ")
+        count+=1
+        if(ch==hideChar):
+            break
+    return count
 
 
 names = filNameList()
 selectName = getRandomName(names)
-print(selectName)
-hideCharForName(selectName)
+hideChar= hideCharForName(selectName)
+count = playing(selectName,hideChar)
 
 
 
